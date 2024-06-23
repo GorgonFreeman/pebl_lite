6c9eaec0e00dcb3f398c216b2008e763a1e1a04d
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-06-24 09:02:53 +1000

    example function

---
+++ exampleFunction.js
@@ -0,0 +1,18 @@
+const exampleFunction = async (message) => {
+  return { 
+    response: `Heard ${ message }`,
+  };
+};
+
+const exampleFunctionApi = async (req, res) => {
+  const { message } = req.body;
+  const result = await exampleFunction(message);
+  respond(res, 200, result);
+};
+
+module.exports = {
+  exampleFunction,
+  exampleFunctionApi,
+};
+
+// curl localhost:8000/exampleFunction -H "Content-Type: application/json" -d '{ "message": "Read the sign - no droids allowed." }'
