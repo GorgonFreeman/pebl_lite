f1107554512f84fbf7dd5ac62c16308befb90d57
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-06-24 18:18:46 +1000

    update Shopify example function

--- shopify/_example.js
+++ shopify/_example.js
@@ -1,14 +1,31 @@
-const { respond } = require('../utils');
+const { respond, shopifyRequestSetup, customAxios } = require('../utils');
 
-const FUNC = async (message) => {
-  return { 
-    response: `Heard ${ message }`,
+const FUNC = async (keyObj, productId, { optionalArg } = {}) => {
+
+  const { url, headers } = shopifyRequestSetup(keyObj);
+
+  const query = `
+    query GetProduct ($id: ID!) {
+      product(id: $id) {
+        ...
+      }
+    }
+  `;
+
+  const variables = {
+    id: `gid://shopify/Product/${ productId }`,
   };
+
+  const result = await customAxios('post', url, { query, variables }, { headers });
+
+  console.log(result);
+
+  return result;
 };
 
 const FUNCApi = async (req, res) => {
-  const { message } = req.body;
-  const result = await FUNC(message);
+  const { keyObj, productId, options } = req.body;
+  const result = await FUNC(keyObj, productId, options);
   respond(res, 200, result);
 };
 
@@ -17,4 +34,4 @@ module.exports = {
   FUNCApi,
 };
 
-// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "message": "Read the sign - no droids allowed." }'
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "keyObj": { "key": "example" }, "productId": "123" }'
