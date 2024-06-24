ecc3d9f2dab7931b9bc7645e4d3efe8311d6ed0e
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-06-24 16:46:58 +1000

    read yaml utility

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const jsYaml = require('js-yaml');
+
 const respond = (res, status, data, contentType = 'application/json') => {
   return res.writeHead(status, { 'Content-Type': contentType }).end(contentType === 'application/json' ? JSON.stringify(data) : data);
 };
@@ -12,7 +14,13 @@ const intsToRangeArray = (min, max) => {
   ];
 };
 
+const readFileYaml = async filePath => {
+  const fileContents = await fs.readFile(filePath, 'utf8');
+  return jsYaml.load(fileContents);
+};
+
 module.exports = {
   respond,
   intsToRangeArray,
+  readFileYaml,
 };
