1bae0694335f905a10525dd98e939c49b8529ce7
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-06-24 17:14:52 +1000

    creds by key function for accessing .env variables along with those stored in creds

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const jsYaml = require('js-yaml');
 const readline = require('readline');
 const fs = require('fs').promises;
@@ -61,6 +62,57 @@ const askQuestion = query => {
 
 const capitaliseString = string => `${ string[0].toUpperCase() }${ string.slice(1) }`;
 
+const credsByKey = ({ platform, key, subkey } = {}) => {
+  const { env } = process;
+
+  if (!platform) {
+    const message = 'credsByKey used with no platform';
+    throw new Error(message);
+  }
+
+  if (!key) {
+    const message = 'credsByKey used with no key';
+    throw new Error(message);
+  }
+
+  let { CREDS } = env;
+  CREDS = JSON.parse(CREDS);
+
+  // console.log(CREDS);
+
+  const keyCreds = CREDS?.[platform]?.[key];
+  const subkeyCreds = CREDS?.[platform]?.[key]?.[subkey];
+
+  if (subkey && !subkeyCreds) {
+    console.warn(`No creds found for ${ platform } > ${ key } > ${ subkey } - will use key-level creds`);
+  }
+
+  if (subkeyCreds) {
+    // Make any arrays into single values - TBD whether this should apply to all creds
+    for (const [k, v] of Object.entries(subkeyCreds)) {
+      if (Array.isArray(v)) {
+        subkeyCreds[k] = v[randomNumber(0, v.length - 1)];
+      }
+    }
+  }
+
+  const creds = {
+    ...keyCreds,
+    ...(subkey ? subkeyCreds : {}),
+  };
+
+  // Keep only uppercase attributes
+  for (const k of Object.keys(creds)) {
+    if (k !== k.toUpperCase()) {
+      delete creds[k];
+    }
+  }
+
+  // console.log(creds);
+
+  return creds;
+};
+
 module.exports = {
   respond,
   intsToRangeArray,
@@ -68,4 +120,5 @@ module.exports = {
   readFileYaml,
   askQuestion,
   capitaliseString,
+  credsByKey,
 };
